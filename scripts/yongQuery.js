09eba95dd3b90f5d03f59751987737c00dda705d
yonghap <yonghapk@gmail.com>
2020-08-24 11:20:58 +0900

    Rest

--- scripts/yongQuery.js
+++ scripts/yongQuery.js
@@ -1,33 +1,6 @@
 // yongQuery 함수
 
-function yongQuery(a,c) {
-	console.log(a);
-	if (window == this) {
-		return new yongQuery(a, c);
-	}
-}
 
-
-// fn, prototype 선언
-yongQuery.fn = yongQuery.prototype = {
-	version : '1.0',
-	size : function() {
-		return this.length;
-	}
-}
-
-
-yongQuery.extend = yongQuery.fn.extend = function (obj,prop) {
-	if ( !prop ) { prop = obj; obj = this; }
-	for ( var i in prop ) obj[i] = prop[i];
-	return obj;
-}
-
-yongQuery.extend({
-	ttt : function () {
-		yongQuery.size();
-	}
-})
 
 
 var $ = yongQuery;
